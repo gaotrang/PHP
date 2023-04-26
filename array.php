f76ec8539aed1f9783b43432e62d4929e46e5572
truonggiang-b2 <ttruonggiangbk@gmail.com>
2023-04-26 21:31:21 +0700

    php buoi2

--- array.php
+++ array.php
@@ -53,11 +53,85 @@ $arrayNumber2 = array(32, 27, 12, 49, 48);
 // Them phan tu vao mang
 $arrayNumber2[] = 13;
 
-unset($arrayNumber2[3]);
+// Xoa phan tu nao do trong mang
+// unset($arrayNumber2[3]);
+
+//Xoa tat ca mang
+// unset($arrayNumber2);
 
 echo '<pre>';
 print_r($arrayNumber2);
 echo '</pre>';
+
+for($i = 0; $i < count($arrayNumber2); $i++){
+    echo $arrayNumber2[$i].'<br>';
+}
+// Tinh tong cac phan tu trong array
+$total = 0;
+for($i = 0; $i < count($arrayNumber2); $i++){
+    $total += $arrayNumber2[$i];
+}
+
+//Cong tat ca cac phan tu la so chan trong array
+$totalEven = 0;
+for($i = 0; $i < count($arrayNumber2); $i++){
+    if($i % 2 === 0){
+        $totalEven += $arrayNumber2[$i];
+    }
+}
+
+$firstOld = 'Khong la so le';
+for($i = 0; $i < count($arrayNumber2); $i++){
+    if($arrayNumber2[$i] % 2 !== 0){
+        $firstOld = $arrayNumber2[$i];
+        break;
+    }
+}
+
+echo '<br>'.$total;
+echo '<br>'.$totalEven;
+echo '<br>'.$firstOld;
+
+echo '<br>';
+// Duyet phan tu bang vong lap WHILE
+$i = 0;
+while($i < count($arrayNumber2)){
+    echo '<br>'.$arrayNumber2[$i];
+    $i++;
+}
+echo '<br>';
+// Duyet phan tu bang vong lap DO WHILE
+$j = 0;
+do {
+    echo '<br>'.$arrayNumber2[$j];
+    $j++;
+}while ($j < count($arrayNumber2));
+
+// Duyet phan tu ko biet so luong phan tu
+echo '<br>';
+foreach($arrayNumber2 as $key=>$value){
+    echo "<br>key : $key value: $value";
+}
+
+   $person = array(
+        "name" => "John",
+        "age" => 30,
+        "city" => "New York"
+    );
+echo '<br>';
+foreach($person as $key=>$value){
+    echo "<br>key : $key value: $value";
+}
+$arrayKeys = array_keys($person);
+$arrayValues = array_values($person);
+
+echo '<pre>';
+print_r($arrayKeys);
+echo '</pre>';
+echo '<pre>';
+print_r($arrayValues);
+echo '</pre>';
+
 
 
 ?>
